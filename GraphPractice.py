dd41caa079200ea20bd482dad49dcda76c95e5da
Wyatt Heller <wyatth88@iastate.edu>
2025-02-06 15:09:08 -0600

    added the unit tests can't get the import line to work to run them, but they exist :(

--- GraphPractice.py
+++ GraphPractice.py
@@ -1,15 +1,57 @@
 from graph import *
-from graph.visuals import plot_2d
-
+from GraphPractice import GameState
 #Create Graph
 g = Graph()
 
 
 #node = vertex
 # dict vtx: {vtxs edges to}
-g.from_list([(1,2), (2,3), (3,4), (4,5), (5,6), (6,1)])
-number = g.nodes()
+#this is C3 to do testing on
+g.from_list([(1,2,0), (2,3, 0), (3,1, 0)])
 
-print(g)
-plot = plot_2d(g)
-plot.show()
+game1 = GameState(g)
+
+#tests
+print("test 1: currentPlayer should be 1, Output true")
+if(game1.currentPlayer()):
+    print("true")
+else:
+    print("false")
+
+print("test 2: isTerminal sould be false, Output false")
+if(game1.isTerminal()):
+    print("true")
+else:
+    print("false")
+
+print("test 3: returning the list of moves, Output (1,2,0), (2,3, 0), (3,1, 0)]")
+print(game1.moves(game1))
+
+print("test 4: Updates current player when advancing the gamestate, Output false")
+game2 = game1.makeMove(game1, "12")
+if(game2.currentPlayer()):
+    print("true")
+else:
+    print("false")
+
+print("test 5: Updates moves when advancing the gamestate, Output [(2,3, 0), (3,1, 0)]")
+print(game2.moves(game2))
+
+print("test 6: Updates currentplayer when updating the gamestate again, Output True")
+game3 = game2.makeMove(game2, "23")
+if(game3.currentPlayer()):
+    print("true")
+else:
+    print("false")
+
+print("test 7: Updates moves when gamestate advances Output none")
+if(game3.moves(game3)==""):
+    print("none")
+else:
+    print(game3.moves(game3))
+
+print("test 8: returns true when games state is termial Output true")
+if(game3.isTerminal()):
+    print("true")
+else:
+    print("false")
